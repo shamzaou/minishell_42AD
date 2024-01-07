ef10e0018c158b63cd89074a5cdc818a397b4b12
legaliab <82210511+legaliab@users.noreply.github.com>
2024-01-08 03:11:45 +0400

    haiwang@haiwang-VirtualBox:~/Desktop/42sh$ git commit -m "add minishell.h"

--- minishell.h
+++ minishell.h
@@ -240,6 +240,9 @@ t_env_var	*env_var_new(char *key, char *value);
 int	env_var_update_value(t_env_var *env_var_list, t_env_var *new_nod);
 void	env_var_insert_sorted(t_env_var **env_var_list, t_env_var *new_node);
 void	init_env_var_list(char **envp, t_env_var **env_var_list);
+void					free_env_var_node(t_env_var *node);
+void					free_env_var_list(t_env_var *env_var_list);
+char					*getenv_value(char *key, t_env_var *env_var_list);
 
 /**
  * SIGNALS
